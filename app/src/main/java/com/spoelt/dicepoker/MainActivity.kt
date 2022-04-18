e5614c4cbcd5462183f77750b9c0977c57691381
spoelt <s.poeltinger@gmail.com>
2022-04-18 18:36:09 +0200

    Fixes #51 - Making system bars transparent (#55)

--- app/src/main/java/com/spoelt/dicepoker/MainActivity.kt
+++ app/src/main/java/com/spoelt/dicepoker/MainActivity.kt
@@ -3,9 +3,13 @@ package com.spoelt.dicepoker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.spoelt.dicepoker.core.BottomBarState
 import com.spoelt.dicepoker.core.ui.NavGraphs
@@ -20,8 +24,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
             val scaffoldState = rememberScaffoldState()
             val navController = rememberNavController()
+
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons
+                )
+            }
 
             DicePokerTheme {
                 Scaffold(

21ebfb99c1ea7614c5fa4d8106dccd1d5c55abb6
spoelt <s.poeltinger@gmail.com>
2022-04-18 17:46:32 +0200

    Updating typography for #52

--- app/src/main/java/com/spoelt/dicepoker/core/ui/theme/Type.kt
+++ app/src/main/java/com/spoelt/dicepoker/core/ui/theme/Type.kt
@@ -1,43 +1,23 @@
 package com.spoelt.dicepoker.core.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
 import com.spoelt.dicepoker.R
 
-private val UbuntuMedium = FontFamily(Font(R.font.ubuntu_medium))
-private val UbuntuLight = FontFamily(Font(R.font.ubuntu_light))
-private val UbuntuRegular = FontFamily(Font(R.font.ubuntu_regular))
+private val UbuntuMedium = Font(R.font.ubuntu_medium, FontWeight.Medium)
+private val UbuntuLight = Font(R.font.ubuntu_light, FontWeight.Light)
+private val UbuntuRegular = Font(R.font.ubuntu_regular, FontWeight.Normal)
+private val UbuntuBold = Font(R.font.ubuntu_bold, FontWeight.Bold)
+
+private val UbuntuFamily = FontFamily(
+    UbuntuMedium,
+    UbuntuLight,
+    UbuntuRegular,
+    UbuntuBold
+)
 
 val typography = Typography(
-    h1 = TextStyle(
-        fontFamily = UbuntuMedium,
-        fontSize = 36.sp
-    ),
-    h2 = TextStyle(
-        fontFamily = UbuntuMedium,
-        fontSize = 24.sp
-    ),
-    h3 = TextStyle(
-        fontFamily = UbuntuRegular,
-        fontSize = 22.sp
-    ),
-    subtitle1 = TextStyle(
-        fontFamily = UbuntuRegular,
-        fontSize = 16.sp
-    ),
-    body1 = TextStyle(
-        fontFamily = UbuntuRegular,
-        fontSize = 14.sp
-    ),
-    button = TextStyle(
-        fontFamily = UbuntuMedium,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = UbuntuLight,
-        fontSize = 12.sp
-    )
+    defaultFontFamily = UbuntuFamily
 )

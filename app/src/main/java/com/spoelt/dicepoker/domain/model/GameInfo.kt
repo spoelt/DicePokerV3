e6a2366f840e082e67e4c400057d04c1f3d154ae
spoelt <s.poeltinger@gmail.com>
2022-07-16 21:14:48 +0200

    Removing Parcelable annotation

--- app/src/main/java/com/spoelt/dicepoker/domain/model/GameInfo.kt
+++ app/src/main/java/com/spoelt/dicepoker/domain/model/GameInfo.kt
@@ -1,11 +1,7 @@
 package com.spoelt.dicepoker.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class GameInfo(
     val numberOfColumns: Int,
     val numberOfPlayers: Int,
     val players: List<Player>
-) : Parcelable
+)
--- app/src/main/java/com/spoelt/dicepoker/domain/model/Player.kt
+++ app/src/main/java/com/spoelt/dicepoker/domain/model/Player.kt
@@ -1,11 +1,7 @@
 package com.spoelt.dicepoker.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Player(
     val playerId: String,
     val name: String? = null,
     val points: List<List<Int>>? = null
-) : Parcelable
+)
